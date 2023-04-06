e8e1bc0f5cff1ee8e7f33da5b1f4e1613f197109
bavaaram <itsjustabavaar@gmail.com>
2023-04-06 04:59:58 +0330

    Add Funny program.py to chapter 1.

---
+++ chapter 1/Funny Program.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+
+if name.lower() == "matin":
+    print("Hello Khodam! ")
+elif name.lower() == "jadi":
+    print("Hello Jadi! Toam Injaii? ")
+else:
+    print("Hello Stranger :/ ")
