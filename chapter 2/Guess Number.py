6df062bad832f2db5dacffc44456c9d2d3bfe22c
bavaaram <itsjustabavaar@gmail.com>
2023-04-09 03:45:22 +0330

    Add some editing codes and debugging for num_check function performance and debugging the code. just finish section has been not designed.

--- chapter 2/Guess Number.py
+++ chapter 2/Guess Number.py
@@ -2,7 +2,7 @@ import random
 
 while 1:
     print("Welcome to number guessing game.")
-    print("*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*")
+    print("*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*\n")
     flag = input(("ready for start? Y/n "))
     charge = 10
 
@@ -18,37 +18,49 @@ while 1:
         print("Exiting the program...")
         break
 
+    def num_check(a):
+        while 1:
+            a = input("Enter your guess: ")
+            if not a.isnumeric():
+                print("Invalid Input, Please try again.")
+                continue
+            if 1 <= int(a) <= 100:
+                break
+            else:
+                print("The entered number is out of range, please try again.")
+                continue
+
     num = random.randint(0, 100)
+    guess = 0
     print("The number is generated.")
     print("Remember, you have 10 chances to guess :)")
     print("************************************************************\n")
     print("%s Guesses remaining" % charge)
-    guess = int(input("Enter your guess: "))
-
+    num_check(guess)
     while 1:
         if charge == 1:
-            if num != guess:
+            if num != int(guess):
                 print("You Lost! ")
                 print("The number is %s" % str(num))
                 break
 
-        if (guess > num) and charge > 1:
+        if (int(guess) > num) and charge > 1:
             charge -= 1
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
             print("The guessed number in greater.")
             print("%s Guesses remaining" % charge)
-            guess = int(input("Enter your guess: "))
+            num_check(guess)
             continue
 
-        if (guess < num) and charge > 1:
+        if (int(guess) < num) and charge > 1:
             charge -= 1
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
             print("The guessed number in smaller.")
             print("%s Guesses remaining" % charge)
-            guess = int(input("Enter your guess: "))
+            num_check(guess)
             continue
 
-        if num == guess:
+        if num == int(guess):
             print("Congrats, you win! ")
             break
 
