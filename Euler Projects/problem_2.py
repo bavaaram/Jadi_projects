90f1e89fa594f170e6528913acabba4467e3e650
bavaaram <itsjustabavaar@gmail.com>
2023-04-10 04:01:45 +0330

    do some cleaning and aditing in problem_2.py file

--- Euler Projects/problem_2.py
+++ Euler Projects/problem_2.py
@@ -2,15 +2,14 @@ fibo = list()
 fibo.append(1)
 fibo.append(2)
 i = 1
+summ = 0
 while fibo[i] < 4000000:
     fibo.append(fibo[i] + fibo[i - 1])
     i += 1
-summ = 0
+
 for j in fibo:
     if j % 2 == 0:
         summ = summ + j
-    else:
-        continue
 
 print(summ)
 
